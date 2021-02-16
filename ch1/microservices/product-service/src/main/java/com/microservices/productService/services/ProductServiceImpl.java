01505639f8234b8d1d8a44ea550251026d0aec61
Shin seongsu <tlstjdtn321@naver.com>
2021-02-16 22:38:45 +0900

    gradle 멀티 프로젝트 설정 해서 RestController 날려보기

---
+++ ch1/microservices/product-service/src/main/java/com/microservices/productService/services/ProductServiceImpl.java
@@ -0,0 +1,23 @@
+package com.microservices.productService.services;
+
+import com.microservices.api.core.product.Product;
+import com.microservices.api.core.product.ProductService;
+import com.microservices.util.http.ServiceUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductServiceImpl implements ProductService {
+
+    private final ServiceUtil serviceUtil;
+
+    @Autowired
+    public ProductServiceImpl(ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;
+    }
+
+    @Override
+    public Product getProduct(int productId) {
+        return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
+    }
+}
