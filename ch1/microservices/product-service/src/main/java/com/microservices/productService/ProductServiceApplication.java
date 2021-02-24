c2a0e24eb2a6c2f25f3eaba0f0e4ca7d7820615e
Shin seongsu <tlstjdtn321@naver.com>
2021-02-24 23:05:49 +0900

    port 오타로 docker에서 오류 나타난것 수정

--- ch1/microservices/product-service/src/main/java/com/microservices/productService/ProductServiceApplication.java
+++ ch1/microservices/product-service/src/main/java/com/microservices/productService/ProductServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com")
+@ComponentScan("com.microservices")
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
--- ch1/microservices/recommendation-service/src/main/java/com/microservices/recommendation/RecommendationServiceApplication.java
+++ ch1/microservices/recommendation-service/src/main/java/com/microservices/recommendation/RecommendationServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@ComponentScan("com")
+@ComponentScan("com.microservices")
 public class RecommendationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RecommendationServiceApplication.class, args);
